671c4fb727cb716fce00e2e83f1ac4323090889b
Abhinav <kotak@ldopa.in>
2026-05-15 12:01:40 +0000

    chore: init devbox

---
+++ flake.nix
@@ -0,0 +1,62 @@
+{
+  description = "My Android project";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+    android.url = "github:tadfisher/android-nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, android }:
+    {
+      overlay = final: prev: {
+        inherit (self.packages.${final.system}) android-sdk android-studio;
+      };
+    }
+    //
+    flake-utils.lib.eachSystem [ "aarch64-darwin" "x86_64-darwin" "x86_64-linux" ] (system:
+      let
+        inherit (nixpkgs) lib;
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+          overlays = [
+            self.overlay
+          ];
+        };
+      in
+      {
+        packages = {
+          android-sdk = android.sdk.${system} (sdkPkgs: with sdkPkgs; [
+            # Useful packages for building and testing.
+            build-tools-36-0-0
+            build-tools-35-0-0
+            cmdline-tools-latest
+            emulator
+            platform-tools
+            platforms-android-36
+
+            # Other useful packages for a development environment.
+            ndk-27-1-12297006
+            cmake-3-22-1
+            # skiaparser-3
+            # sources-android-34
+          ]
+          ++ lib.optionals (system == "aarch64-darwin") [
+            # system-images-android-34-google-apis-arm64-v8a
+            # system-images-android-34-google-apis-playstore-arm64-v8a
+          ]
+          ++ lib.optionals (system == "x86_64-darwin" || system == "x86_64-linux") [
+            # system-images-android-34-google-apis-x86-64
+            # system-images-android-34-google-apis-playstore-x86-64
+          ]);
+        } // lib.optionalAttrs (system == "x86_64-linux") {
+          # Android Studio in nixpkgs is currently packaged for x86_64-linux only.
+          android-studio = pkgs.androidStudioPackages.stable;
+          # android-studio = pkgs.androidStudioPackages.beta;
+          # android-studio = pkgs.androidStudioPackages.preview;
+          # android-studio = pkgs.androidStudioPackage.canary;
+        };
+      }
+    );
+}
